169e4c2799c7ffbdd20241d3f90d366f09468649
asimolpiq <alpayguroglu@gmail.com>
2022-08-05 06:17:14 +0300

    first commit

--- lib/product/padding.dart
+++ lib/product/padding.dart
@@ -4,6 +4,6 @@ class ProjectPaddings {
   final EdgeInsets minPadding = const EdgeInsets.all(5);
   final EdgeInsets midPadding = const EdgeInsets.all(10);
   final EdgeInsets maxPadding = const EdgeInsets.all(20);
-  final EdgeInsets boxPadding = const EdgeInsets.symmetric(vertical: 30, horizontal: 5);
+  final EdgeInsets boxPadding = const EdgeInsets.symmetric(vertical: 20, horizontal: 5);
   final BorderRadius radius20 = BorderRadius.circular(20);
 }
